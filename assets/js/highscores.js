a3728034f8e40144989fb7d14469b57b19dd5e88
Nick Deignan <nickdeignan@Nicks-MacBook-Pro.local>
2023-08-31 14:10:47 -0600

    basic framework and function: created index.html and highscores.html, basic css styling code, created javascript code to run quiz, store user highscores to local storage, and retrieve the highscore data

---
+++ assets/js/highscores.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const highscoresList = document.getElementById("highscores-list");
+  const clearScoresButton = document.getElementById("clear-scores");
+
+  // Retrieve highscores from local storage
+  const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
+
+  // Display highscores in the list
+  highscores.forEach((entry, index) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${index + 1}. ${entry.initials} - ${entry.score}`;
+    highscoresList.appendChild(listItem);
+  });
+
+  // Clear scores button click handler
+  clearScoresButton.addEventListener("click", () => {
+    localStorage.removeItem("highscores");
+    highscoresList.innerHTML = ""; // Clear displayed list items
+  });
+});
